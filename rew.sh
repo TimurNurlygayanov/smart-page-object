6aa4762f54e594990843b4380b972cab17cecac5
Timur Nurlygayanov <timurnurlygayanov@mail.ru>
2019-01-08 17:54:38 +0200

    Added script to fix github history

---
+++ rew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git filter-branch --env-filter '
+OLD_EMAIL="t.nurlygayaianov@semrush.com"
+CORRECT_NAME="TimurNurlygayanov"
+CORRECT_EMAIL="timurnurlygayanov@mail.ru"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
